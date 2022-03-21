3a0f9bca63474cd64fba940728873e8cfe009d08
Filip Curkovic <filip.curkovic1@gmail.com>
2022-03-21 23:57:07 +0100

    init(workspace): #2 setup husky hooks & setup commit message linter

--- .husky/commit-msg.ts
+++ .husky/commit-msg.ts
@@ -6,10 +6,6 @@ if (!commitMsg) {
   throw new Error();
 }
 
-// console.log('COMIT MSG AA ', process.argv);
-// console.log('COMIT MSG ', commitMsg.pop());
-// console.log('REGEX ', regex);
-// console.log('REGEX ', regex);
 console.log(regex?.test(commitMsg));
 // if (!regex.test(commitMsg)) {
 //   console.error('Invalid commit message format. Correct format: "feat(scope): #issue_num <commit_msg>"');
