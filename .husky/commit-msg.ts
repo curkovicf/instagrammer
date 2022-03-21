5374fe4251eabf4e40fee1cf7ebb51e8f7012d28
Filip Curkovic <filip.curkovic1@gmail.com>
2022-03-21 23:39:28 +0100

    invlaid

--- .husky/commit-msg.ts
+++ .husky/commit-msg.ts
@@ -1,11 +1,11 @@
-// const pattern = '/^(feat|fix|docs|init|test|refactor|ci|revert)(\(.+?\))?: (#[1-9]\d*) [\s\S]*/g';
-const commitMsg = process.argv;
+const regex = new RegExp(/^(feat|fix|docs|init|test|refactor|ci|revert)(\(.+?\))?: (#[1-9]\d*) [\s\S]*/g);
+const commitMsg = process.argv.pop();
 
 // console.log('COMIT MSG AA ', process.argv);
-console.log('COMIT MSG ', commitMsg.pop());
+// console.log('COMIT MSG ', commitMsg.pop());
 
-// if (!(/^(feat|fix|docs|init|test|refactor|ci|revert)((.+?))?: (#[1-9]d*) [sS]*/g.test(commitMsg))) {
-//   console.error('Invalid commit message format. Correct format: "feat(scope): #issue_num <commit_msg>"');
-//   // process.exitCode = 9;
-// }
+if (!regex.test(commitMsg)) {
+  console.error('Invalid commit message format. Correct format: "feat(scope): #issue_num <commit_msg>"');
+  // process.exitCode = 9;
+}
 
