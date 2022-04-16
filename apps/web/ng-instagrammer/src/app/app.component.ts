7e613ad1796694f317d980a3c406fa130f95724f
curkovicf <filip.curkovic1@gmail.com>
2022-04-16 14:57:14 +0200

    feat(ng-auth): #7 minor updates to the login screen styles

--- apps/web/ng-instagrammer/src/app/app.component.ts
+++ apps/web/ng-instagrammer/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'instagrammer-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'web-ng-instagrammer';
+  constructor(private readonly title: Title) {
+    this.title.setTitle('Instagram');
+  }
 }
