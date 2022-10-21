0c548c3488f9ff3ca9ee2b63816c092a3ef7ef83
Filip Ćurković <49046940+curkovicf@users.noreply.github.com>
2022-10-21 20:52:50 +0200

    feat(feed): #19 setup post dto, frontend checkpoint

--- libs/api/feed/data-access/src/index.ts
+++ libs/api/feed/data-access/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/post.module';
 export * from './lib/service/post.service';
+export * from './lib/dto/create-post.dto';
 export * from './lib/service/post.interface';
--- libs/api/feed/data-access/src/lib/service/post.service.ts
+++ libs/api/feed/data-access/src/lib/service/post.service.ts
@@ -4,6 +4,7 @@ import { PostEntity } from '@instagrammer/api/core/data-access';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PostRepository } from '../repository/post.repository';
 import { UserRepository } from '@instagrammer/api/auth/data-access';
+import { CreatePostDto } from '../dto/create-post.dto';
 
 @Injectable()
 export class PostService implements IPostService {
@@ -17,5 +18,9 @@ export class PostService implements IPostService {
       where: { user: { userId: userId } },
       select: { postId: true, comments: true, description: true, likes: true, createdAt: true, photoPaths: true },
     });
+  }
+
+  public createPost(userId: string, createPostDto: CreatePostDto): Promise<void> {
+    return Promise.resolve(undefined);
   }
 }
