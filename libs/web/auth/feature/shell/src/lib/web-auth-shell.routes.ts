f04040e49a07824b4405a4c13bcbf5f835ab3a61
Filip Ćurković <filip.curkovic1@gmail.com>
2022-12-03 19:28:30 +0100

    feat(feed): #27 fixed routing behavior

--- libs/web/auth/feature/shell/src/lib/web-auth-shell.routes.ts
+++ libs/web/auth/feature/shell/src/lib/web-auth-shell.routes.ts
@@ -5,22 +5,27 @@ export const authRoutes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    // redirectTo: '/auth/login',
-    pathMatch: 'full',
-  },
-  {
-    path: 'login',
-    // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-    loadChildren: async () => (await import('@instagrammer/web/auth/feature/login')).LoginModule,
-  },
-  {
-    path: 'register',
-    // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-    loadChildren: async () => (await import('@instagrammer/web/auth/feature/register')).RegisterModule,
-  },
-  {
-    path: 'onetap',
-    // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-    loadChildren: async () => (await import('@instagrammer/web-auth-feature-save-login-info')).SaveLoginInfoModule,
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+        loadChildren: async () => (await import('@instagrammer/web/auth/feature/login')).LoginModule,
+      },
+      {
+        path: 'register',
+        // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+        loadChildren: async () => (await import('@instagrammer/web/auth/feature/register')).RegisterModule,
+      },
+      {
+        path: 'onetap',
+        // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+        loadChildren: async () => (await import('@instagrammer/web-auth-feature-save-login-info')).SaveLoginInfoModule,
+      },
+    ],
   },
 ];
