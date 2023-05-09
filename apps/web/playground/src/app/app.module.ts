eeb278dfd6ecb18c385cc0578715f750bc34136d
Filip Ćurković <filip.curkovic1@gmail.com>
2023-05-09 20:51:15 +0200

    feat(collapsible): #37 init boilerplate

--- apps/web/playground/src/app/app.module.ts
+++ apps/web/playground/src/app/app.module.ts
@@ -6,10 +6,17 @@ import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { TextAreaComponent } from '@instagrammer/web/shared/ui/text-area';
 import { TextAreaRichComponent } from '@instagrammer/web/shared/ui/text-area-rich';
+import { CollapsableComponent } from '@instagrammer/web/shared/ui/collapsable';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes), TextAreaComponent, TextAreaRichComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    TextAreaComponent,
+    TextAreaRichComponent,
+    CollapsableComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ libs/web/shared/ui/collapsable/src/lib/collapsable.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'inst-s-collapsable',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './collapsable.component.html',
+  styleUrls: ['./collapsable.component.scss'],
+})
+export class CollapsableComponent {}
