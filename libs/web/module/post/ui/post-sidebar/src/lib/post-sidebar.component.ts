c517b5d099bb6e483a28dcea3d7a112e62d48da4
Filip Ćurković <filip.curkovic1@gmail.com>
2023-05-14 23:49:21 +0200

    feat(content-posting): #20 impl styles for 3rd step, wired up components

--- libs/web/module/post/ui/post-sidebar/src/lib/post-sidebar.component.ts
+++ libs/web/module/post/ui/post-sidebar/src/lib/post-sidebar.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TextAreaComponent } from '@instagrammer/web/shared/ui/text-area';
+import { TextAreaRichComponent } from '@instagrammer/web/shared/ui/text-area-rich';
+import { InputLocationComponent } from '@instagrammer/web/shared/ui/input-location';
+import { CollapsableComponent } from '@instagrammer/web/shared/ui/collapsable';
 
 @Component({
   selector: 'ng-inst-post-sidebar',
   standalone: true,
-  imports: [CommonModule, TextAreaComponent],
+  imports: [
+    CommonModule,
+    TextAreaComponent,
+    TextAreaRichComponent,
+    InputLocationComponent,
+    CollapsableComponent,
+  ],
   templateUrl: './post-sidebar.component.html',
   styleUrls: ['./post-sidebar.component.scss'],
 })
