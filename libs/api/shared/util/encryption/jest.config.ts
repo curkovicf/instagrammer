5db0954cb6a45d00da390df9d0c0fba5cf31205c
Filip Ćurković <filip.curkovic1@gmail.com>
2023-11-08 22:49:49 +0100

    refactor(auth): #46 refactored api lib types

--- libs/api/shared/util/encryption/jest.config.ts
+++ libs/api/shared/util/encryption/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'api-shared-data',
-  preset: '../../../../jest.preset.js',
+  displayName: 'api-shared-util-encryption',
+  preset: '../../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../../coverage/libs/api/shared/data',
+  coverageDirectory: '../../../../../coverage/libs/api/shared/util/encryption',
 };
